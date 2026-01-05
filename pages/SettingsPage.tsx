679cfce170ebd1c95d879f3fbc6813bfb09c1348
amalec1021 <artur.malec@apptimia.com>
2026-01-05 12:31:42 +0100

    Add icons to settings page

--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 
 export default SettingsPageContent;
 
@@ -10,15 +10,42 @@ function SettingsPageContent() {
       </View>
 
       <View style={styles.content}>
-        <Pressable style={styles.button} onPress={() => {console.log("My profile pressed")}}>
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            console.log("My profile pressed");
+          }}
+        >
+          <Image
+            source={require("../assets/icon.png")}
+            style={styles.settingIcon}
+          />
           <Text style={styles.buttonText}>My profile</Text>
         </Pressable>
 
-        <Pressable style={styles.button} onPress={() => {console.log("Account management pressed")}}>
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            console.log("Account management pressed");
+          }}
+        >
+            <Image
+            source={require("../assets/icon.png")}
+            style={styles.settingIcon}
+          />
           <Text style={styles.buttonText}>Account management</Text>
         </Pressable>
 
-        <Pressable style={styles.buttonLogOut} onPress={() => {console.log("Log out pressed")}}>
+        <Pressable
+          style={styles.buttonLogOut}
+          onPress={() => {
+            console.log("Log out pressed");
+          }}
+        >
+            <Image
+            source={require("../assets/icon.png")}
+            style={styles.settingIcon}
+          />
           <Text style={styles.buttonText}>Log out</Text>
         </Pressable>
       </View>
@@ -42,23 +69,34 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
-    content: {
+  content: {
     flex: 1,
   },
-    button: {
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#999494ff",
   },
-    buttonText: {
+  buttonText: {
     fontSize: 16,
   },
   buttonLogOut: {
+    flexDirection: "row",
+    alignItems: "center",
     marginTop: 30,
     padding: 16,
     borderBottomWidth: 1,
     borderTopWidth: 1,
     borderBottomColor: "#999494ff",
     borderTopColor: "#999494ff",
+  },
+  settingIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 6,
+    marginRight: 12,
+    backgroundColor: "#e0e0e0",
   },
 });
